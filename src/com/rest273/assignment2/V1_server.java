0243ff7bcbebdf007a0d77cbf97e07e3c9f78da3
jianwei <jhan0530@gmail.com>
2015-06-30 10:58:03 -0700

    6/30

--- src/com/rest273/assignment2/V1_server.java
+++ src/com/rest273/assignment2/V1_server.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.Response;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -21,72 +22,56 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 import com.rest273.data.*;
-
-
 import com.rest273.assignment2.ItemEntry;
 import com.rest273.data.MysqlClient;
+import com.rest273.data.QueryDriver;
 import com.rest273.data.SchemaMySql;
 import com.rest273.util.ToJSON;
 
-
 @Path("/V1/server/")
 public class V1_server {
-
-	static JSONArray jsonarray = new JSONArray();
 
 	@GET
 	@Path("/get")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTrackInJSON() throws Exception {
-		
+
 		Response rb = null;
-		
-		DBCollection dBCollection = QueryDriver.getmongoDbCollection("zaneacademydatabase", "Channel"); 
-		
+
+		DBCollection dBCollection = QueryDriver.getmongoDbCollection(
+				"zaneacademydatabase", "Channel");
+
 		BasicDBObject basicDBObject = new BasicDBObject();
 		basicDBObject.put("name", "zaneacademy");
 		DBCursor dbCursor = dBCollection.find(basicDBObject);
-		if(dbCursor.hasNext()) 
-		{
+		if (dbCursor.hasNext()) {
 			String next = dbCursor.next().toString();
 			System.out.println(next);
 			rb = Response.ok(next).build();
 		}
 		return rb;
 	}
-/*		
-		
-		PreparedStatement query = null;
-		Connection conn = null;
-		String returnString = null;
-		Response rb = null;
-		
-		try{
-			conn = MysqlClient.MySqlCom2Conn().getConnection();
-			query = conn.prepareStatement("select * from wearable.clientinfo");
-			ResultSet rs = query.executeQuery();
-			
-			ToJSON converter = new ToJSON();
-			JSONArray json = new JSONArray();
-			
-			json = converter.toJSONArray(rs);
-			query.close(); //close connection
-			
-			returnString = json.toString();
-			rb = Response.ok(returnString).build();
-			
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		finally{
-			if (conn != null) conn.close();
-		}
-		
-		return rb;
-	}
-*/
-	
+
+	/*
+	 * 
+	 * PreparedStatement query = null; Connection conn = null; String
+	 * returnString = null; Response rb = null;
+	 * 
+	 * try{ conn = MysqlClient.MySqlCom2Conn().getConnection(); query =
+	 * conn.prepareStatement("select * from wearable.clientinfo"); ResultSet rs
+	 * = query.executeQuery();
+	 * 
+	 * ToJSON converter = new ToJSON(); JSONArray json = new JSONArray();
+	 * 
+	 * json = converter.toJSONArray(rs); query.close(); //close connection
+	 * 
+	 * returnString = json.toString(); rb = Response.ok(returnString).build();
+	 * 
+	 * } catch(Exception e){ e.printStackTrace(); } finally{ if (conn != null)
+	 * conn.close(); }
+	 * 
+	 * return rb; }
+	 */
 
 	@POST
 	@Path("/post")
@@ -94,40 +79,47 @@ public class V1_server {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response createTrackInJSON(String incomingData) throws Exception {
 
+		
+		String returnString = null;
+        DBCollection dBCollection = QueryDriver.getmongoDbCollection("lwm2mserver", "registerclient");
+            
+        QueryDriver.insertJSON(incomingData,dBCollection);
+		
+		returnString = "2.01 Created";
+		
+		/*
 		String returnString = null;
 		JSONArray json = new JSONArray();
 		SchemaMySql dao = new SchemaMySql();
-		/*
-		try {
-			System.out.println("incomingData: " + incomingData);
-			
+
+		 * try { System.out.println("incomingData: " + incomingData);
+		 * 
+		 * 
+		 * 
+		 * ObjectMapper mapper = new ObjectMapper(); ItemEntry itemEntry =
+		 * mapper.readValue(incomingData, ItemEntry.class);
+		 * 
+		 * int http_code = dao.insertIntoPC_PARTS(itemEntry.clientID,
+		 * itemEntry.times);
+		 * 
+		 * if( http_code == 200 ) { json =
+		 * dao.queryReturnbrandParts(itemEntry.clientID); returnString =
+		 * json.toString();
+		 * 
+		 * } else { return
+		 * Response.status(500).entity("Unable to process Item").build(); }
+		 * 
+		 * } catch (Exception e) { e.printStackTrace(); return
+		 * Response.status(500
+		 * ).entity("Server was not able to process your request").build(); }
+		 */
 		
-			
-			ObjectMapper mapper = new ObjectMapper();  
-			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
-			
-			int http_code = dao.insertIntoPC_PARTS(itemEntry.clientID, itemEntry.times);
-			
-			if( http_code == 200 ) {
-				json = dao.queryReturnbrandParts(itemEntry.clientID);
-				returnString = json.toString();
-				
-			} else {
-				return Response.status(500).entity("Unable to process Item").build();
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Response.status(500).entity("Server was not able to process your request").build();
-		}
-		
-		*/
-		returnString = incomingData;
 		return Response.status(201).entity(returnString).build();
 	}
 
 }
-class ItemEntry{
+
+class ItemEntry {
 	public String clientID;
 	public String times;
 }
--- src/com/rest273/client/V1_client.java
+++ src/com/rest273/client/V1_client.java
@@ -1,5 +1,7 @@
 package com.rest273.client;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import com.sun.jersey.api.client.Client;
@@ -7,14 +9,187 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class V1_client {
+	
+	private JSONArray objectList;
+	private String uRN;
+	private String bootServerURL;
+/*
+	public V1_client(JSONArray  objectList)
+	{
+		this.objectList=objectList;
+	}
+*/	
 
-	public static void main(String[] args) {
-		ClientObject client = new ClientObject("client1", 0);
-
-		clientRest(client);
-
+	public String getURN() {
+		return uRN;
 	}
 
+
+	public void setURN(String uRN) {
+		this.uRN = uRN;
+	}
+
+
+	public JSONArray getObjectList() {
+		return objectList;
+	}
+
+
+	public void setObjectList(JSONArray objectList) {
+		this.objectList = objectList;
+	}
+
+
+
+	
+	private JSONArray initialServer() throws JSONException {
+		
+		JSONArray serverArray = new JSONArray();
+		
+		JSONObject object = new JSONObject();
+		object.put("ObjectName","LWM2M Server");
+		object.put("ObjectID","1");
+		object.put("ObjectInstanceID","0");
+		serverArray.put(object);
+		
+		JSONObject URI = new JSONObject();
+		URI.put("ResourceName","LWM2M Server URI");
+		URI.put("ResourceID","0");
+		URI.put("ResourceInstanceID","");
+		URI.put("Value","");
+		serverArray.put(URI);
+				
+		return serverArray;
+	}
+	
+	private JSONArray initialDevice() throws JSONException {
+		
+		JSONArray deviceArray = new JSONArray();
+		
+		JSONObject object = new JSONObject();
+		object.put("ObjectName","Device");
+		object.put("ObjectID","3");
+		object.put("ObjectInstanceID","0");
+		deviceArray.put(object);
+		
+		JSONObject manufacturer = new JSONObject();
+		manufacturer.put("ResourceName","Manufacturer");
+		manufacturer.put("ResourceID","0");
+		manufacturer.put("ResourceInstanceID","");
+		manufacturer.put("Value","LG");
+		deviceArray.put(manufacturer);
+		
+		JSONObject mondelNumber = new JSONObject();
+		mondelNumber.put("ResourceName","Model Number");
+		mondelNumber.put("ResourceID","1");
+		mondelNumber.put("ResourceInstanceID","");
+		mondelNumber.put("Value","Smart Refrigerator 1.0");
+		deviceArray.put(mondelNumber);
+		
+		JSONObject serialNumber = new JSONObject();
+		serialNumber.put("ResourceName","Serial Number");
+		serialNumber.put("ResourceID","2");
+		serialNumber.put("ResourceInstanceID","");
+		serialNumber.put("Value","zx90003k31");
+		deviceArray.put(serialNumber);
+		return deviceArray;
+	}
+	
+	public void bootStrap() throws JSONException{
+		System.out.println("Start to boot and try to register to bootstrap server.");
+		if (bootServerURL!=null){
+			
+			Client client = Client.create();
+			WebResource webResource = client.resource(bootServerURL);
+			
+			JSONObject uRNobject = new JSONObject();
+			uRNobject.put("clientESN_URN", uRN);
+			ClientResponse response = webResource.type("application/json").post(ClientResponse.class, uRNobject);
+			
+			if (response.getStatus() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
+			
+			JSONObject responseJson = new JSONObject(response.getEntity(String.class));
+			
+			objectList.getJSONArray(0).getJSONObject(1).remove("Value");
+			objectList.getJSONArray(0).getJSONObject(1).put("Value",responseJson.getString("serverURI"));
+			System.out.println("Boot successfully! LWM2M Server URI is: " + responseJson.getString("serverURI"));
+		}
+		else{
+			System.out.println("Can not find bootstrap server URL. Bootstrap failed!");
+		}
+	}
+	
+	
+	public void register() throws JSONException{
+		System.out.println("Start to register to LWM2M server.");
+		String m2mServerURL = objectList.getJSONArray(0).getJSONObject(1).getString("Value");
+		if (m2mServerURL!=null){
+			
+			m2mServerURL += "/post";
+			
+			Client client = Client.create();
+			WebResource webResource = client.resource(m2mServerURL);
+			JSONObject registerObject = new JSONObject();
+			registerObject.put("Endpoint Client Name", uRN);
+			
+			JSONArray JsonArray = new JSONArray();
+			JSONObject iterObject;
+			String objectinsID;
+			for(int i=0;i < objectList.length();i++)
+			{
+				objectinsID=objectList.getJSONArray(i).getJSONObject(0).getString("ObjectID") + "/"+objectList.getJSONArray(i).getJSONObject(0).getString("ObjectInstanceID");
+			
+				iterObject=new JSONObject();
+				iterObject.put("n", objectinsID);
+				JsonArray.put(iterObject);
+			}
+			
+			registerObject.put("e",JsonArray);
+			
+             ClientResponse response = webResource.type("application/json").post(ClientResponse.class, registerObject);
+			
+			if (response.getStatus() != 201) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
+			
+			System.out.println("Response from LWM2M server: " + response.getEntity(String.class));
+		}
+		else {
+			System.out.println("Can not find LWM2M server URL. Register failed!");
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) throws JSONException {
+		
+
+		V1_client client = new V1_client();
+		client.setURN("00000002");
+		client.bootServerURL="http://localhost:8080/com.rest273.bootstrapServer/rest/V1/server/post";
+		
+		JSONArray serverArray = new JSONArray();
+		JSONArray deviceArray = new JSONArray();
+		JSONArray objectList = new JSONArray();
+		serverArray = client.initialServer();
+		deviceArray = client.initialDevice();
+		objectList.put(serverArray);
+		objectList.put(deviceArray);
+		client.setObjectList(objectList);
+		client.bootStrap();
+		System.out.println(objectList.getJSONArray(0).getJSONObject(1).getString("Value"));
+		client.register();				
+
+	}
+	
+	
+	
+	
+	
 	public static void clientRest(ClientObject clientInput) {
 		try {
 
